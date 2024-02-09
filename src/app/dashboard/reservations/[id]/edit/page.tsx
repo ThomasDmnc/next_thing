12e3a76a510a90fbe3d380da519bc64c71d399b5
ThomasDmnc <thomas.demoncy@gmail.com>
2024-02-09 17:41:24 +0100

    fixing typo

--- src/app/dashboard/reservations/[id]/edit/page.tsx
+++ src/app/dashboard/reservations/[id]/edit/page.tsx
@@ -3,13 +3,13 @@ import { getReservation } from '../../../../lib/actions'
 import { useEffect, useState } from 'react';
 import { Reservation } from '@/app/lib/definitions';
 
-export default function EditReservation({ params }: { params: { id: string }){ 
+export default function EditReservation({ params }: { params: { id: string }}){ 
     const id = params.id;
     const [reservation, setReservation] = useState<Reservation | null>(null); 
     
     useEffect(()=> {
         getReservation(id).then(setReservation)
-    })
+    }, [id])
 
     return (
         <>
