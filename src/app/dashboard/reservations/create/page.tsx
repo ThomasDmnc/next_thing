3c55e475191741f57320036397cb16a3432bbaf6
ThomasDmnc <thomas.demoncy@gmail.com>
2024-02-12 15:20:27 +0100

    Added creation form no refacto atm

--- src/app/dashboard/reservations/create/page.tsx
+++ src/app/dashboard/reservations/create/page.tsx
@@ -1,5 +1,12 @@
+import NewForm from '../../../ui/reservations/new-form'
+
 export default function CreateReservation(){
     return(
-        <h1>Create</h1>
+        <main className="flex min-h-screen flex-col w-screen items-start py-8 md:py-12 lg:py-12 px-4 md:px-12">
+            <div className='flex flex-row justify-center items-start max-w-6xl'>
+                <h1 className='text-4xl font-bold text-blue-600'>Create a new reservation:</h1>
+            </div>
+            <NewForm />
+        </main>
     )
 }
